37764d0ccfad2303e6832997381559b39439ec29
ishikapandeyy <ishikapandey.workmail@gmail.com>
2026-09-25 11:37:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let timeDisply=document.getElementById("timer")
+let newDate=new Date();
+setInterval(
+	function(){
+		newDate=new Date();
+		timeDisply.innerHtml=newDate.toLocalString();
+	},1000
+)
